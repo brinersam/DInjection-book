e003b1e6de9067354e4522c8aa8da673ed5d8b04
brinersam <vladbudyachenko@mail.ru>
2024-05-08 21:45:38 +0300

    HelloDi looked in book to see how new logic gets added to the decorator class

--- 01_HelloDi/Classes.cs
+++ 01_HelloDi/Classes.cs
@@ -3,27 +3,33 @@ namespace HelloDi
     class PrinterWithAuth : IPrints
     {
         IPrints _printer;
+        User _user;
         int _passwordGoal = 123;
         int _passwordLast;
-        public PrinterWithAuth(IPrints printer)
+        public PrinterWithAuth(IPrints printer, User usr)
         {
             _printer = printer;
-        }
-
-        public void EnterPassword(int pass)
-        {
-            _passwordLast = pass;
+            _user = usr;
         }
 
         public void Print()
         {
-            if (_passwordLast != _passwordGoal)
-            {
-
+            if (_user.IsAuthenticated == false) // he adds new logic by composing a new class which has all the logic
+            {   // this way u dont need to fuck with interface-implemented-methods, neither u need any new overloads
+                // pretty cool huh
                 Console.WriteLine("Access Denied!");
                 return;
             }
             _printer.Print();
+        }
+    }
+    class User
+    {
+        int _pass;
+        public bool IsAuthenticated => _pass == 123;
+        public User(int password)
+        {
+            _pass = password;
         }
     }
     class Printer : IPrints
--- 01_HelloDi/main.cs
+++ 01_HelloDi/main.cs
@@ -2,19 +2,16 @@ namespace HelloDi
 {
     public class Main : IRunnable
     {
-        public void Run()
+        public void Run() //shut the fuck up roslyn
         {
             IMessage msg = new Message("Hello world");
             string printerName = File.ReadLines("./01_HelloDi/BootFromText.txt").FirstOrDefault();
             
-            IPrints printer = (IPrints)Activator.CreateInstance
+            IPrints printer = (IPrints)Activator.CreateInstance // late binding
                                             (Type.GetType(printerName),msg);
             printer.Print();
 
-            printer = new PrinterWithAuth(printer); // Open closed expansion
-            if (printer is PrinterWithAuth)
-                (printer as PrinterWithAuth).EnterPassword(3);
-
+            printer = new PrinterWithAuth(printer, new User(2)); // Open closed expansion
             printer.Print();
         }
     }
